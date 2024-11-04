a49c82ab93dd4dc0c06a695dacaaa6ac7d42f25b
dreamer981 <sdreamer590@gmail.com>
2024-11-04 22:08:06 +0300

    tekrar düzenlendi

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 function getElementWidth (content, padding, border) {    
-    console.log(parseInt(content) + 2*parseInt(padding) + 2*parseFloat(border))
+    return parseInt(content) + 2*parseInt(padding) + 2*parseFloat(border);
 }
 
-getElementWidth("50px", "8px", "4px"); // 74
-getElementWidth("60px", "12px", "8.5px"); // 101
-getElementWidth("200px", "0px", "0px"); // 200
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
